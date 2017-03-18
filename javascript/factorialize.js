884a555e18e549cae18a699607fa691ed576370c
Andrew Jarrett <ahrjarrett@gmail.com>
2017-03-17 19:13:10 -0600

    wrote tail-recursive factorialize

--- javascript/factorialize.js
+++ javascript/factorialize.js
@@ -2,19 +2,43 @@
 
 // First thought is, could use a reduce.
 // Second thought is, could be recursive.
-function factorialize(num) {
+// Third thought is, it should be tail recursive.
+function reduce_factorial (n) {
+
 
 }
 
-function factorializeImperative (num) {
+// RECURSIVE (TAIL)
+const recur_factorial = (n) => _fact(n, 1)
+
+const _fact = (n, a) =>
+  n === 1 ?
+    a
+    : _fact(n - 1, n * a)
+
+
+//function recur_factorial (n) {
+//  return _recur_helper (n, 1)
+//}
+
+//function _recur_helper (n, a) {
+//  // why return a, and not 1?
+//  if(n === 1) return a
+//  else return _recur_helper(n - 1, n * a)
+//}
+
+console.log(recur_factorial(5))
+console.log(recur_factorial(1))
+
+
+function imperativeFactorialize (n) {
   var total = 1
-  while(num > 0) {
-    total *= num
-    num--
+  while(n > 0) {
+    total *= n
+    n--
   }
   return total
 }
 
-// factorialize(5) should equal 120
 
-module.exports = factorialize
+module.exports = recur_factorial
