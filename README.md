5eb9d015f699c5240c5dcad1c0e2b17046aa6f9b
Andrew Jarrett <ahrjarrett@gmail.com>
2017-03-18 00:07:17 -0600

    finished palindrome, updated readme to refactor later

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Solutions to various interesting freeCodeCamp problems.
 
 ### Notes
-- Palindrome algorithm incomplete (03/17/2017)
+- ~~Palindrome algorithm incomplete (03/17/2017)~~
+- Palindrome algorithm complete, refactor (03/18/2017)
--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -1,17 +1,14 @@
-function palindrome(str) {
-  return test(str)
-}
-function test(str) {
-  console.log(str
 
-    .split('')
-      .replace(/\s/, '')
-    )
+// TODO: Clean this up! Very imperative / method-heavy
+const palindrome = (str) => {
+  const cleaned = str.replace(/[\W]+/gi, '').toLowerCase().split('')
+  if(cleaned.join('') === cleaned.reverse().join('')) return true
+  else return false
 }
-
 
 console.log(palindrome('racecar'))
-console.log(palindrome('not a palindrome'))
+console.log(palindrome('Olivia Tremor Control'))
+console.log(palindrome('Ten animals I slam in a net.'))
 
 module.exports = palindrome
 
