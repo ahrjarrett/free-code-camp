479204f0e0666ae88fcd07375385a39819235478
Andrew Jarrett <ahrjarrett@gmail.com>
2017-04-15 16:13:02 -0600

    project random quote ready for first submission

--- projects/random-quote-tweet/public/javascripts/main.js
+++ projects/random-quote-tweet/public/javascripts/main.js
@@ -1,9 +1,11 @@
+'use strict'
 
 // DOM
 var new_quote = document.getElementById('get-quote')
 var quote_body = document.getElementById('quote-body')
 var quote_author = document.getElementById('quote-author')
 var tweet = document.getElementById('twitter-share-button')
+var tweet_warning = document.getElementById('tweet-warning')
 
 // AJAX
 new_quote.addEventListener('click', function(e) {
@@ -17,20 +19,14 @@ new_quote.addEventListener('click', function(e) {
   request.success(function(data) {
     var quote = data.shift()
     var content = quote.content
-    var title = quote.title
-
+    var author = quote.title
+    // Have to do this hacky trimming b/c response comes back within p tags and appends a \n character
     var trimmed_quote = content
           .slice(3, content.length - 5)
           .trim()
-          //.split(' ')
-          //.join('%20')
 
     quote_body.innerHTML = content
-    quote_author.innerHTML = title
-
-    console.log(trimmed_quote)
-    tweet.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + trimmed_quote)
+    quote_author.innerHTML = author
+    tweet.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(trimmed_quote + ' —' + author))
   })
-
 })
-
