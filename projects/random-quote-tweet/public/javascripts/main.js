54b23d8560e510e90781395c9ca3759b2372f04b
Andrew Jarrett <ahrjarrett@gmail.com>
2017-04-15 13:58:23 -0600

    got quote generator to work with API call

--- projects/random-quote-tweet/public/javascripts/main.js
+++ projects/random-quote-tweet/public/javascripts/main.js
@@ -1,27 +1,22 @@
-// constants
-var request = $.ajax({
-  url: 'test',
-  dataType: 'json'
-})
 
 // DOM
 var new_quote = document.getElementById('get-quote')
 var quote_body = document.getElementById('quote-body')
 var quote_author = document.getElementById('quote-author')
 
+// AJAX
 new_quote.addEventListener('click', function(e) {
   e.preventDefault()
+
+  var request = $.ajax({
+    url: `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=`,
+    cache: false
+  })
+  request.success(function(data) {
+    var quote = data.shift()
+    quote_body.innerHTML = quote.content
+    quote_author.innerHTML = quote.title
+  })
+
 })
 
-// Ajax
-request.done(function(quotes) {
-  var quote = quotes
-  console.log(quotes)
-  return quotes
-})
-
-request.fail(function(e) {
-  alert('Error! ' + e)
-})
-
-console.log(new_quote)
