05a36f16242414b17e40e097de29e1bdb72575f2
Andrew Jarrett <ahrjarrett@gmail.com>
2017-04-15 21:39:00 -0600

    intermediate algorithms: sum of all in a range

---
+++ javascript/intermediate-algorithm-scripting/sum-of-range.js
@@ -0,0 +1,16 @@
+const sumAll = (arr) => {
+  let sorted = arr.sort(function(a, b) {
+    return a - b;
+  })
+  let range = []
+
+  while(sorted[0] <= sorted[1]) {
+    range.push(sorted[0])
+    sorted[0]++
+  }
+  return range.reduce((acc, curr) => { return acc + curr }, 0)
+}
+
+console.log(sumAll([1, 4])) // 10
+
+module.exports = sumAll
